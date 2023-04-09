2cbceb22dedadc14731c3215a492f58d136d5e28
@hexlet/cli <support@hexlet.io>
2023-04-09 09:31:33 +0300

    submit java-web-ru/session

--- java-web-ru/session/src/main/java/exercise/servlet/SessionServlet.java
+++ java-web-ru/session/src/main/java/exercise/servlet/SessionServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 import static exercise.App.getUsers;
@@ -67,7 +68,9 @@ public class SessionServlet extends HttpServlet {
         } else {
             session.setAttribute("flash", "Неверные логин или пароль");
             response.setStatus(422);
-            request.setAttribute("email", email);
+            Map<String, String> incorrectUser = new HashMap<>();
+            incorrectUser.put("email", email);
+            request.setAttribute("user", incorrectUser);
             RequestDispatcher requestDispatcher = request.getRequestDispatcher("/login.jsp");
             requestDispatcher.forward(request, response);
         }
